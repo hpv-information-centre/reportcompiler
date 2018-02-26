715a62e20880bb7aa3bd38f734cc5a97b874c998
David Gomez <dgomez@iconcologia.net>
2018-02-26 10:17:30 +0100

    Add LICENSE and README.rst stub

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 
 os.chdir(
     os.path.normpath(
-	    os.path.join(os.path.abspath(__file__), os.pardir)))
+        os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='report-compiler',
