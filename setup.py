a3f33ae69c0cf9b3b4d8addb0f7c7921c8f5cd4c
David Gomez <dgomez@iconcologia.net>
2018-03-15 16:27:13 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(
 
 setup(
     name='reportcompiler',
-    version='0.3.1',
+    version='0.3.2',
     packages=find_packages('.'),
     include_package_data=True,
     license='MIT License',
