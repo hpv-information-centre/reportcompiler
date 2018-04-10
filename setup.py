110980f99fffd6bdc74e96ca6c27d1a2b5a26dd1
David Gomez <dgomez@iconcologia.net>
2018-04-10 17:04:03 +0200

    Update version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(
 
 setup(
     name='reportcompiler',
-    version='0.3.2',
+    version='0.3.3',
     packages=find_packages('.'),
     include_package_data=True,
     license='GPL-3.0 License',
