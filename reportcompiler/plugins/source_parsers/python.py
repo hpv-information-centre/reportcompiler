f770ccc02f488d557dacdaf170598632612116af
David Gomez <dgomez@iconcologia.net>
2018-04-27 12:39:08 +0200

    Fix metadata retrieval for only jsonable fields

--- reportcompiler/plugins/source_parsers/python.py
+++ reportcompiler/plugins/source_parsers/python.py
@@ -28,6 +28,13 @@ class PythonParser(SourceParser):
         return context
 
     def retrieve_fragment_metadata(self, doc_var, metadata):
+        def is_jsonable(x):
+            try:
+                json.dumps(x)
+                return True
+            except:
+                return False
+
         module_name = metadata['fragment_name']
 
         fragment_module = None
@@ -39,6 +46,5 @@ class PythonParser(SourceParser):
 
         module_vars = {var: fragment_module.__dict__[var]
                        for var in dir(fragment_module)
-                       if (not callable(fragment_module.__dict__[var]) and
-                           not var.startswith('__'))}
+                       if (is_jsonable(fragment_module.__dict__[var]))}
         return module_vars
