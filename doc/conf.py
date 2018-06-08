66e3483c528f7f164b74c05bde4da73622fa6e0f
David Gomez <dgomez@iconcologia.net>
2018-06-08 14:25:55 +0200

    Change PDF documentation style

--- doc/conf.py
+++ doc/conf.py
@@ -175,7 +175,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -275,6 +275,7 @@ latex_elements = {
      # Latex figure (float) alignment
      #
      # 'figure_align': 'htbp',
+     'extraclassoptions': ',openany,oneside'
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
